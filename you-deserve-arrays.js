35e9cb4f34b42e8d9ed8819e1e23d7cd5a8a7d5a
Hans Vos <hans.vos@codeimmersives.com>
2019-10-24 09:22:36 -0400

    git bullied

--- you-deserve-arrays.js
+++ you-deserve-arrays.js
@@ -1,25 +1,33 @@
 function getFirstItemFrom(arr) {
-  return arr[1];
+  return arr[0];
 }
 
 function getLastItemFrom(arr) {
-  return arr.length - 1;
+  return array[arr.length - 1];
 }
 
 function getIndex3(arr) {
-  if (arr.length < 4) {
+  if(arr.length >= 4) {
+    return arr[3];
+  } else if (arr.length <= 4) {
     return getLastItemFrom(arr);
-  }
-
-  return arr[4];  
+  } 
 }
 
 function firstItemIsNumber(arr) {
-  return getFirstItemFrom(arr) === 'number'
+  if(arr[0] === Number(arr[0])) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function isLongList(arr) {
-  return arr.length >= 10 && arr.length < 20;
+  if(arr.length >= 10) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
